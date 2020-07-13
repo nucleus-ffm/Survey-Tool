a5677d18757335dbd05e71c3ab161d5ba6624508
Nucleus <Nucleus-ffm@posteo.de>
2020-07-13 14:03:27 +0200

    init components-dropdown

---
+++ js/components-dropdown.js
@@ -0,0 +1,9 @@
+
+// When the user clicks on <div>, open the popup
+function toggelComponents() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show2");
+}
+
