de0ed4319163da517f0a8c881aea130c5eac52d4
Nucleus <Nucleus-ffm@posteo.de>
2020-07-17 13:03:28 +0200

    feat: add footer

--- footer.php
+++ footer.php
@@ -1,3 +1,26 @@
-<h1>hier kommt der footer hin</h1>
 </body>
+<!-- end body -->
+<!--start footer-->
+<footer>
+    <!-- beginn site-footer. -->
+    <div class="site-footer">
+        <nav class="footer-navigation">
+            <div class="footer-menue-container">
+                <ul class="footer-menue">
+                    <li><a href=""> Impressum</a> </li>
+                    <li><a href=""> Datenschutz</a> </li> 
+                    <li><a href=""> Kontakt</a></li> 
+                    <li><a href=""> Über</a></li>
+                </ul>  
+            </div>    
+        </nav>
+    </div>
+    <!-- end site-footer -->
+    <!-- beginn credits -->
+    <div class="credits">
+        <p>Author: Lilsemy and Nucleus</p>
+        <p><a class="email" href="mailto:hege@example.com">hege@example.com</a></p>
+    </div>
+    <!-- end credits -->
+</footer> 
 </html>
