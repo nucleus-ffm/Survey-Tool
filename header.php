89afde3bd953d4c11ba195bb8ddde6e129991ff7
Nucleus <Nucleus-ffm@posteo.de>
2020-07-17 13:07:49 +0200

    feat: moved js to main.js

--- header.php
+++ header.php
@@ -1,43 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Survey-Tool</title>
-    <link rel="stylesheet" href="css/main.css"/>
-
-    <!-- Dropdown Menue -->
-  	<div class="dropdown">
-       <button onclick="toggleMenue()" class="">Einstellungen</button>
-        <div id="myDropdown" class="dropdown-content">
-            <a href="profil.php">Profil</a>
-            <a href="settings.php">Einstellungen</a>
-            <a class="logout" href="logout.php">logout</a>
-        </div>
-  	</div>
-
-  <script type="text/javascript">
-    /* When the user clicks on the button,
-    toggle between hiding and showing the dropdown content */
-    function toggleMenue() {
-      document.getElementById("myDropdown").classList.toggle("show");
-    }
-
-    // Close the dropdown menu if the user clicks outside of it
-    window.onclick = function(event) {
-      if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    }
-  </script>
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Survey-Tool</title>
+  <!-- begin import css -->
+  <link rel="stylesheet" href="css/main.css"/>
+  <link rel="stylesheet" href="css/form.css"/>
+  <link rel="stylesheet" href="css/footer.css"/>
+  <!-- end import css -->
+  <!-- start dropdown menue -->
+  <div class="dropdown">
+      <button onclick="toggleMenue()" class="">Einstellungen</button>
+      <div id="myDropdown" class="dropdown-content">
+          <a href="profil.php">Profil</a>
+          <a href="settings.php">Einstellungen</a>
+          <a class="logout" href="logout.php">logout</a>
+      </div>
+  </div>
+  <!-- end dropdown menue-->
+  <!-- beginn import js -->
+  <script src="js/main.js"></script>
+  <!-- end imoort js -->
 </head>
 <body>
