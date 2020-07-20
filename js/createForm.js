bda84c98b687924a99c07595354de370a9430bba
Nucleus <Nucleus-ffm@posteo.de>
2020-07-20 21:52:11 +0200

    feat: add radio-element

--- js/createForm.js
+++ js/createForm.js
@@ -1,15 +1,16 @@
 /*global vars*/
 var NumberOfElements = 0; 
+var NumberOfDivs = 0; 
 /*begin DOM*/
 function createTextBox() {
-    var DivID = countElements();
+    var DivID = countDivs();
     const myForm = document.getElementById('myForm');
     let newDiv = document.createElement('div');
     newDiv.className ="formElementDiv";
-    newDiv.id = DivID;
+    newDiv.id = DivID+ "-Div";
     myForm.appendChild(newDiv);
 
-    const myDiv = document.getElementById(DivID);
+    const myDiv = document.getElementById(DivID+ "-Div");
     let newElement = document.createElement('textarea');
     let deleteButton = document.createElement('button');
     deleteButton.id = DivID + "-deleteButton";
@@ -20,11 +21,22 @@ function createTextBox() {
     newElement.placeholder = "Hier bitte Text eintragen";
     newElement.className = "myFormElemente";
 
+    let svgObject = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svgObject.setAttribute('class', 'newElement');
+    svgObject.setAttribute('width', '24');
+    svgObject.setAttribute('height', '24');
+    svgObject.setAttribute('viewBox', '-2 -2 24 24');
+    svgObject.setAttribute('role', 'img');
+    svgObject.setAttribute('aria-hidden' , 'true');
+    svgObject.setAttribute('focusable' , 'false');
+    svgObject.innerHTML = '<path d="M10 1c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7zm1-11H9v3H6v2h3v3h2v-3h3V9h-3V6zM10 1c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7zm1-11H9v3H6v2h3v3h2v-3h3V9h-3V6z"></path>';
+
     myDiv.appendChild(newElement);
     myDiv.appendChild(deleteButton);
+    myDiv.appendChild(svgObject);
 }
 function createTextInput() {
-    var DivID = countElements();
+    var DivID = countDivs();
     const myForm = document.getElementById('myForm');
     let newDiv = document.createElement('div');
     newDiv.className ="formElementDiv";
@@ -45,7 +57,7 @@ function createTextInput() {
     myDiv.appendChild(deleteButton);
 }
 function createRadioButton() {
-    var DivID = countElements();
+    var DivID = countDivs();
     const myForm = document.getElementById('myForm');
     let newDiv = document.createElement('div');
     newDiv.className ="formElementDiv";
@@ -53,26 +65,51 @@ function createRadioButton() {
     myForm.appendChild(newDiv);
 
     const myDiv = document.getElementById(DivID);
-    let newRadioButton = document.createElement('text');
-    newRadioButton.textContent ="Hallo Welt";
+
+    let newControlDiv = document.createElement('div');
+    newControlDiv.className ="RadioControlDiv";
+    newControlDiv.id = DivID +"-control";
+    myDiv.appendChild(newControlDiv);
+
+    const controlDiv = document.getElementById(DivID + "-control");
+
+    let plus = document.createElement('button');
+    plus.textContent = "add Radio Button";
+    plus.className ="greenButton";
+    plus.id = DivID; 
+    plus.setAttribute("onclick", "newRadioButton(this.id)");
+
+    /*let svgObject = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svgObject.setAttribute("onclick", "newRadioButton(this.parentNode.id)");
+    svgObject.setAttribute('class', 'newElement');
+    svgObject.setAttribute('width', '24');
+    svgObject.setAttribute('height', '24');
+    svgObject.setAttribute('viewBox', '-2 -2 24 24');
+    svgObject.setAttribute('role', 'img');
+    svgObject.setAttribute('aria-hidden' , 'true');
+    svgObject.setAttribute('focusable' , 'false');
+    svgObject.innerHTML = '<path d="M10 1c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7zm1-11H9v3H6v2h3v3h2v-3h3V9h-3V6zM10 1c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7zm1-11H9v3H6v2h3v3h2v-3h3V9h-3V6z"></path>';
+    */
 
     let deleteButton = document.createElement('button');
-    deleteButton.id = DivID + "-deleteButton";
+    deleteButton.className ="deleteElement";
+    deleteButton.id = DivID;
     deleteButton.textContent ="Delete Element";
-    deleteButton.setAttribute("onclick", "deleteElement(this.parentNode.id)");
-
-    myDiv.appendChild(newRadioButton);
-    myDiv.appendChild(deleteButton);
+    deleteButton.setAttribute("onclick", "deleteElement(this.id)");
+    
+    controlDiv.appendChild(plus);
+    controlDiv.appendChild(deleteButton);
+    
 }
 function createRangeSlider() {
-    var DivID = countElements();
+    var DivID = countDivs();
     const myForm = document.getElementById('myForm');
     let newDiv = document.createElement('div');
     newDiv.className ="formElementDiv";
-    newDiv.id = DivID;
+    newDiv.id = DivID + "-Div";
     myForm.appendChild(newDiv);
 
-    const myDiv = document.getElementById(DivID);
+    const myDiv = document.getElementById(DivID+ "-Div");
     let inputElement = document.createElement('input');
     inputElement.id = DivID + "-item";
     inputElement.type ="range";
@@ -103,7 +140,7 @@ function createRangeSlider() {
     myDiv.appendChild(deleteButton);
 }
 function createKeyword() {
-    var DivID = countElements();
+    var DivID = countDivs();
     const myForm = document.getElementById('myForm');
     let newDiv = document.createElement('div');
     newDiv.className ="formElementDiv";
@@ -127,7 +164,16 @@ function createKeyword() {
 function deleteElement(id) {
     let element = document.getElementById(id);
     element.parentNode.removeChild(element);
-    /*TODO:move IDs by one*/
+    /*TODO:move IDs by one */
+    console.log("movedElement: " +id++);
+    let movedElement = document.getElementById(id++);
+    let movedElement2 = document.getElementById(id + 2);
+    console.log("movedElement2: " +id + 2);
+    var buffer = 0;
+    buffer = movedElement.id;
+    movedElement.id = id-1;
+    movedElement2.id = buffer;
+    
 }
 
 /*delete all*/
@@ -145,14 +191,21 @@ function create10 () {
         createTextInput();
     }
 }
-/*count elements*/
+/*count Divs*/
+function countDivs() {
+    NumberOfNextDiv = NumberOfDivs;
+    NumberOfDivs++; 
+    console.log(NumberOfDivs);
+    return NumberOfNextDiv;
+}
+/*count Elements*/
 function countElements () {
     NumberOfNextElement = NumberOfElements;
     NumberOfElements++; 
     console.log(NumberOfElements);
     return NumberOfNextElement;
 }
-
+/*just for testing */
 window.onmouseover=function(e) {
     console.log(e.target.id);
 };
@@ -165,5 +218,51 @@ function saveSilderElement (id) {
     const end = document.getElementById(id + "-endInput");
     endvalue = end.value;
     silder.setAttribute("max", endvalue);
+}
 
+function newRadioButton (id) {
+    ElementID = countElements();
+    
+    const myDiv = document.getElementById(id);
+    let newDiv = document.createElement('div');
+    newDiv.className ="formRadioDiv";
+    newDiv.id = id + ElementID;
+    myDiv.appendChild(newDiv);
+
+    const myRadioDiv = document.getElementById(id + ElementID);
+    let newRadioButton = document.createElement('input');
+    newRadioButton.type = "radio";
+    newRadioButton.id = ElementID + "-radio";
+    newRadioButton.name = id;
+    let radioButtonText = document.createElement('text');
+    radioButtonText.textContent ="--";
+    radioButtonText.id = ElementID +"-Text";
+    let RadioButtonValue = document.createElement('input');
+    RadioButtonValue.id = ElementID + "-input";
+    RadioButtonValue.placeholder ="Bitte Wert eintragen";
+    let saveButton = document.createElement('button'); 
+    saveButton.className ="greenButton";
+    saveButton.textContent ="save";
+    saveButton.id = ElementID;
+    saveButton.setAttribute("onclick", "saveRadioButtonValue(this.id)");
+    let deleteButton = document.createElement('button');
+    deleteButton.className ="deleteElement";
+    deleteButton.id = ElementID + "-deleteButton";
+    deleteButton.textContent ="Delete";
+    deleteButton.setAttribute("onclick", "deleteElement(this.parentNode.id)");
+    myRadioDiv.appendChild(newRadioButton);
+    myRadioDiv.appendChild(radioButtonText);
+    myRadioDiv.appendChild(RadioButtonValue);
+    myRadioDiv.appendChild(saveButton);
+    myRadioDiv.appendChild(deleteButton);
+}
+
+function saveRadioButtonValue (id) {
+    console.log("saveRadioButtonValue"+ id);
+    const myRadioButtonText = document.getElementById(id +"-Text");
+    const myRadioButton = document.getElementById(id+ "-radio");
+    const myInputValue = document.getElementById(id+"-input");
+    inputValue = myInputValue.value;
+    myRadioButtonText.textContent = inputValue;
+    myRadioButton.value = inputValue;
 }
