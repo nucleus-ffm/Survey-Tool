a07e1de86dd0bd57135e0d20e71b3be42438903c
Nico <Nico-Git@psoteo.de>
2020-07-24 20:52:27 +0200

    feat: add welcome text

--- landing.php
+++ landing.php
@@ -1,6 +1,12 @@
-<div class="center">
-    <input placeholder="Please input ID">
+<div class="welcome"> 
+    <!-- page info -->
+    <p> <strong>Willkommen</strong> auf der Umfrageseite der ****. Wenn Du an einer Umfrage teilnehmen möchtest, gibt bitte unten diese ID der Umfrage ein. 
+    informationen, wie Du selbst Umfragen erstellen kannst, findest du <a href="">hier</a>.
+    </p>
+
+</div>
+<div class="center landing">
+    <input autofocus placeholder="Please input ID">
     <button>los</button>
     <br>
-    <a href="createForm.html">zum Formular</a>
 </div>
