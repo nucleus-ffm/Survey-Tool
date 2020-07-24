e3adf7fd6fedf8fa3f6aa5fd59d0d6523c52b907
Nico <Nico-Git@psoteo.de>
2020-07-24 20:50:54 +0200

    feat: add dashboard

---
+++ dashboard.php
@@ -0,0 +1,44 @@
+<?php
+include("header.php");
+ ?>
+<div>
+  <div class="dashboard">
+    <a href= "createForm.php"><button class="dashboard-button"> Neue Umfrage</button></a>
+    <div class="dashboard-table-div">
+      <table class=table>
+        <tr>
+          <th>Title</th>
+          <th>gestartet</th>
+          <th>Auswertung</th>
+          <th>control</th>
+        </tr>
+        <tr>
+          <th>Wie schön ist die ERS1?</th>
+          <th> 24.06.2020</th>
+          <th>24 Teilnehmer</th>
+            <th> <button>bearbeiten</button> <button>Auswerung</button> <button>löschen</button>
+        </tr>
+        <tr>
+          <th>Brauchen wir besseres Essen in der Mensa?</th>
+          <th> 14.07.2020</th>
+          <th>24 Teilnehmer</th>
+            <th> <button>bearbeiten</button> <button>Auswerung</button> <button>löschen</button>
+        </tr>
+        <tr>
+          <th>Wer ist die beste Lehrerin?</th>
+          <th> 11.06.2020</th>
+          <th>72 Teilnehmer</th>
+            <th> <button>bearbeiten</button> <button>Auswerung</button> <button>löschen</button>
+        </tr>
+        <tr>
+          <th>Wer ist die beste Lehrerin?</th>
+          <th> 11.06.2020</th>
+          <th>72 Teilnehmer</th>
+            <th> <button>bearbeiten</button> <button>Auswerung</button> <button>löschen</button>
+        </tr>
+    </div>
+  </div>
+</div>
+ <?php
+ include("footer.php");
+ ?>
