7a043c496a43a3b1ae38db84742fe9281fef94d2
Nucleus <Nucleus-ffm@posteo.de>
2020-08-03 14:45:33 +0200

    feat: md css form header.php

---
+++ head.php
@@ -0,0 +1,21 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+//$_SESSION['loggedIn'] = TRUE;
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Survey-Tool</title>
+  <!-- begin import css -->
+  <link rel="stylesheet" href="css/main.css"/>
+  <link rel="stylesheet" href="css/form.css"/>
+  <link rel="stylesheet" href="css/footer.css"/>
+  <link rel="stylesheet" href="css/header.css"/>
+  <link rel="stylesheet" href="css/dashboard.css"/>
+  <link rel="stylesheet" href="css/components-dropdown.css"/>
+  <!-- end import css -->
+</head>
