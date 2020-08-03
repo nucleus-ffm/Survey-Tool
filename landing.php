514c0196e6648b7cb4a9710b22121e940053ac53
Nucleus <Nucleus-ffm@posteo.de>
2020-08-03 14:41:39 +0200

    feat: some small changes

--- landing.php
+++ landing.php
@@ -1,17 +1,25 @@
+<!-- landing Page 
+Content: 
+    - input Field to join Survey
+-->
 <div class="content">
+    <!-- beginn input field to join Survey -->
     <div class="welcome"> 
         <!-- page info -->
         <p> <strong>Willkommen</strong> auf der Umfrageseite der ****. Wenn Du an einer Umfrage teilnehmen möchtest, gibt bitte unten diese ID der Umfrage ein. 
         informationen, wie Du selbst Umfragen erstellen kannst, findest du <a href="">hier</a>.
         </p>
-
     </div>
-    <div class="center">
+    <div class="IDinput">
         <form class="landing" action="join.php" method="GET">
-        <input name="ID" autofocus placeholder="Please input ID">
-        <input class="landingSubmit" type="submit" value="join">
-        <br>
+            <input required name="ID" autofocus placeholder="Please input ID">
+            <input class="landingSubmit" type="submit" value="join">
+            <br>    
         </form>
+    <a href="landingPage/index.php"><button class="roundButton">Erfahre hier mehr über uns </button> </a>
     </div>
-
-</div>
+    </div>
+    <!-- end -->
+<?php
+include("footer.php");
+?>
