1c5ef5ddb2184371dc6aa1c0a98feb95132241ea
Nucleus <Nucleus-ffm@posteo.de>
2020-08-03 14:42:40 +0200

    feat: add ID input

--- join.php
+++ join.php
@@ -1,5 +1,29 @@
 <?php
-include("header.php");
+include("head.php");
+if (!isset($_GET['ID'])) {
+    ?>
+    <div class="content">
+    <div class="welcome"> 
+        <!-- page info -->
+        <p> <strong>Willkommen</strong> auf der Umfrageseite der ****. Wenn Du an einer Umfrage teilnehmen möchtest, gibt bitte unten diese ID der Umfrage ein. 
+        informationen, wie Du selbst Umfragen erstellen kannst, findest du <a href="">hier</a>.
+        </p>
+
+    </div>
+    <div class="center">
+        <form class="landing" action="join.php" method="GET">
+        <input required name="ID" autofocus placeholder="Please input ID">
+        <input class="landingSubmit" type="submit" value="join">
+        <br>
+        </form>
+    </div>
+
+    </div>
+    <?php
+    include("footer.php");
+    exit;
+}
+
 $surveyID = $_GET['ID'];
 require_once("config.php");
 $getSurvey = "SELECT * FROM surveys WHERE surveyID = $surveyID";
@@ -145,3 +169,6 @@ if ($result->num_rows > 0) {
 </form> 
 <!-- inport JS Script -->
 <script src="js/answerForm.js"></script>
+<?php
+include("footer.php");
+?>
