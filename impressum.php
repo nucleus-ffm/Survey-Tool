f7d16e0eb8c92c4e80e128137b082d152703e933
Nucleus <Nucleus-ffm@posteo.de>
2020-08-03 14:43:50 +0200

    feat: add impressum

---
+++ impressum.php
@@ -0,0 +1,42 @@
+<?php
+//include CSS and JS
+include("head.php");
+//include NAV bar
+include("header.php");
+?>
+<body>
+<div class="content">
+    <div class="impressum">
+        <h1>Impressum </h1>
+        <h2>Angaben gemäß § 5 TMG </h2>
+        <p>
+            Max Mustermann <br>
+            Musterstraße 1a <br>
+            45039 Berlin Deutschland
+        </p> 
+        <h2>Kontakt</h2>
+        <p>
+            FAX: 12345678 /78 <br>
+            Email: user@provider.de <br>
+            Internet: Internetseite 
+        </p>
+        <h2>Adressverarbeitung </h2>
+        <p>
+            Die hier angegebenen kontaktdaten dürfen nicht für die zusendung von Werbung genutzt werden. 
+        </p>
+        <h2>Urheberrecht </h2>
+        <p>
+            Die uns erstellten Inhalte auf dieser Seite unterliegen dem Deutschen Urheberrecht. Jegliches Kopieren bedarf einer vorherigen
+            schriftlichen Zustimmung. 
+        </p>
+        <h2>Lizenz </h2>
+        <p>
+            Die hier eingesetzte Software ist openSource unterliegt aber noch keine Lizenz. Gern kann der Source Code eingesehen werden. 
+            Ein kopieren ist zu dem jetztigen Zeitpunkt allerdings noch nicht erlaubt. 
+        </p>
+    </div>
+</div>
+</body>
+<?php
+include("footer.php");
+?>
