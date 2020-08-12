cf2c0b25680c73645c104ee6aebfad148678648f
Nucleus <Nucleus-ffm@posteo.de>
2020-08-12 14:08:06 +0200

    feat: add privacy template

---
+++ privacy.php
@@ -0,0 +1,191 @@
+<?php
+//include CSS and JS
+include("head.php");
+//include NAV bar
+include("header.php");
+?>
+<body>
+<div class="content">
+    <div class="impressum-privacy">
+        <h1>Datenschutzerklärung </h1>
+<div>
+<p>
+    <strong>
+        DIES IST NUR EINE VORLAGE UND KEINESFALLS RECHTSSICHER. AUCH KANN DIESE DATENSCHUTZERKLÄRUNG UNVOLLSTÄNDIG SEIN. 
+        WIR BIETEN NUR DIESE VORLAGE AN. DIE URPRÜNGLICH VON:
+        Professor Dr. Thomas Hoeren zusammen mit Mitarbeitern der
+        Forschungsstelle Recht des DFN-Vereins entwickelt wurde.
+    </strong>
+</p>
+<br>
+<p>
+    Da für uns selbst der Datenschutz sehr wichtig ist und wir es selber nicht leiden können, wenn die eigenen Daten immer an mehrere Drittanbieter weiter gegeben werden,
+    liegt unser eigenes Bestreben darin, diese Webseite und das Forum so datenschutzfreundlich wie möglich zu gestalten. Wenn Ihr noch weitere Tipps habt, was wir verbessern können,
+    schreibt uns gern eine E-Mail an: datenschutz [ät] PROVIDER.de
+    <br><br>Aus rechtlichen Gründen hier die vollständige Datenschutzerklärung.
+</p>
+<h2>Name und Anschrift des Verantwortlichen</h2>
+<p>
+    Der Verantwortliche im Sinne der Datenschutz-Grundverordnung und anderer nationaler Datenschutzgesetze der Mitgliedsstaaten sowie sonstiger datenschutzrechtlicher Bestimmungen ist die:
+</p>
+<br>
+<p>Organisationsname</p>
+<p>
+    Musterstaße 1e<br>
+    60325 Frankfurt am Main<br>
+    Deutschland<br>
+    Tel.: 0000 12345678
+</p>
+<p>
+    E-Mail: info [ät]PROVIDER.de <br>
+    Website: www.SEITE.com
+</p>
+
+<p>
+    <br>Sollten Sie noch weitere Fragen zum Datenschutz auf unserer Webseite haben, können Sie sich auch gern unter der unten stehenden E-Mail Adresse melden.
+    <br><br>
+    <em>E-Mail: datenschutz@PROVIDER.de</em>
+</p>
+<p><em>Website: www.SEITE.de</em></p>
+
+<h2> Allgemeines zur Datenverarbeitung </h2>
+
+<p>Wir verarbeiten personenbezogene Daten unserer Nutzer grundsätzlich nur, soweit dies zur Bereitstellung einer funktionsfähigen Website sowie unserer Inhalte und Leistungen erforderlich ist. Die Verarbeitung personenbezogener Daten unserer Nutzer erfolgt regelmäßig nur nach Einwilligung des Nutzers. Eine Ausnahme gilt in solchen Fällen, in denen eine vorherige Einholung einer Einwilligung aus tatsächlichen Gründen nicht möglich ist und die Verarbeitung der Daten durch gesetzliche Vorschriften gestattet ist. </p>
+
+<h2>1. Rechtsgrundlage für die Verarbeitung personenbezogener Daten </h2>
+
+<p>Soweit wir für Verarbeitungsvorgänge personenbezogener Daten eine Einwilligung der betroffenen Person einholen, dient Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;a EU-Datenschutzgrundverordnung (DSGVO) als Rechtsgrundlage.</p>
+<p>Bei der Verarbeitung von personenbezogenen Daten, die zur Erfüllung eines Vertrages, dessen Vertragspartei die betroffene Person ist, erforderlich ist, dient Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;b DSGVO als Rechtsgrundlage. Dies gilt auch für Verarbeitungsvorgänge, die zur Durchführung vorvertraglicher Maßnahmen erforderlich sind.</p>
+<p>Soweit eine Verarbeitung personenbezogener Daten zur Erfüllung einer rechtlichen Verpflichtung erforderlich ist, der unser Unternehmen unterliegt, dient Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;c DSGVO als Rechtsgrundlage.</p> <p>Für den Fall, dass lebenswichtige Interessen der betroffenen Person oder einer anderen natürlichen Person eine Verarbeitung personenbezogener Daten erforderlich machen, dient Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;d DSGVO als Rechtsgrundlage.</p>
+<p>Ist die Verarbeitung zur Wahrung eines berechtigten Interesses unseres Unternehmens oder eines Dritten erforderlich und überwiegen die Interessen, Grundrechte und Grundfreiheiten des Betroffenen das erstgenannte Interesse nicht, so dient Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;f DSGVO als Rechtsgrundlage für die Verarbeitung</p>
+
+<h3> Datenlöschung und Speicherdauer </h3>
+
+<p>
+    Die personenbezogenen Daten der betroffenen Person werden gelöscht oder gesperrt,
+    sobald der Zweck der Speicherung entfällt. Eine Speicherung kann darüber hinaus erfolgen,
+    wenn dies durch den europäischen oder nationalen Gesetzgeber in unionsrechtlichen Verordnungen,
+    Gesetzen oder sonstigen Vorschriften, denen der Verantwortliche unterliegt, vorgesehen wurde.
+    Eine Sperrung oder Löschung der Daten erfolgt auch dann, wenn eine durch die genannten Normen vorgeschriebene Speicherfrist abläuft,
+    es sei denn, dass eine Erforderlichkeit zur weiteren Speicherung der Daten für einen Vertragsabschluss oder eine Vertragserfüllung besteht.
+</p>
+
+<h2> 2. Bereitstellung der Website und Erstellung von Logfiles </h2>
+
+<p>Bei jedem Aufruf unserer Internetseite erfasst unser System automatisiert Daten und Informationen vom Computersystem des aufrufenden Rechners. </p>
+<p>Folgende Daten werden hierbei erhoben:</p>
+<ol>
+    <li> Informationen über den Browsertyp und die verwendete Version </li>
+    <li> Das Betriebssystem des Nutzers </li>
+    <li> Den Internet-Service-Provider des Nutzers </li>
+    <li> Die IP-Adresse des Nutzers </li>
+    <li> Datum und Uhrzeit des Zugriffs </li>
+    <li> Websites, von denen das System des Nutzers auf unsere Internetseite gelangt </li>
+    <li> Websites, die vom System des Nutzers über unsere Website aufgerufen werden </li>
+</ol> 
+
+<h3> Rechtsgrundlage für die Datenverarbeitung </h3>
+<p>Rechtsgrundlage für die vorübergehende Speicherung der Daten ist Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;f DSGVO. </p>
+
+<h3> Zweck der Datenverarbeitung </h3> 
+<p>Die vorübergehende Speicherung der IP-Adresse durch das System ist notwendig, um eine Auslieferung der Website an den Rechner des Nutzers zu ermöglichen. Hierfür muss die IP-Adresse des Nutzers für die Dauer der Sitzung gespeichert bleiben. </p>
+<h3> Dauer der Speicherung </h3>
+<p>Die Daten werden gelöscht, sobald sie für die Erreichung des Zweckes ihrer Erhebung nicht mehr erforderlich sind. Zusätzlich können Logfiles zu Analysezwecken gespeichert werden. Diese Daten liegen jedoch alle auf unserem Server und werden NICHT mit Drittanbietern geteilt oder zu Werbezwecken genutzt. </p>
+<h3> Widerspruchs- und Beseitigungsmöglichkeit </h3>
+<p>Die Erfassung der Daten zur Bereitstellung der Website und die Speicherung der Daten in Logfiles ist für den Betrieb der Internetseite zwingend erforderlich. Es besteht folglich seitens des Nutzers keine Widerspruchsmöglichkeit. Der dauerhaften Speicherung kann jedoch widersprochen werden. </p>
+
+<h2> 3. Registrierung / Anmeldung</h2>
+<p>
+    Auf unserer Internetseite bieten wir Nutzern die Möglichkeit, sich unter Angabe personenbezogener Daten zu registrieren.
+    Die Daten werden dabei in eine Eingabemaske eingegeben und an uns übermittelt und gespeichert.
+    Eine Weitergabe der Daten an Dritte findet nicht statt.
+    Folgende Daten werden im Rahmen des Registrierungsprozesses erhoben:
+</p>
+<p> Im Zeitpunkt der Registrierung werden zudem folgende Daten gespeichert:</p>
+<ol>
+    <li> Die IP-Adresse des Nutzers </li>
+    <li> Datum und Uhrzeit der Registrierung </li>
+    <li>Nutzername</li>
+    <li>E-Mail Adresse</li>
+    <li>kurze Begründung</li>
+</ol>
+
+<h3> Zweck der Datenverarbeitung </h3>
+<p>
+    Eine Anmeldung ist für das Erstellen von Umfragen Notwendig
+</p>
+
+<h3> Dauer und Art der Speicherung</h3>
+<p>
+    Die Daten werden bei uns in der Datenbank gespeichert und werden gelöscht,
+    sobald sie für die Erreichung des Zweckes ihrer Erhebung nicht mehr erforderlich sind.
+    Das heißt bei der löschung des Accounts.
+</p> 
+<h2>4. Cookies</h2>
+<p>
+    Wenn du einen Kommentar auf unserer Website schreibst, kann das eine Einwilligung sein, deinen Namen, E-Mail-Adresse und Website in Cookies zu speichern. Dies ist eine Komfortfunktion, damit du nicht, wenn du einen weiteren Kommentar schreibst, all diese Daten erneut eingeben musst. Diese Cookies werden ein Jahr lang gespeichert.</p> 
+<p>Falls du ein Konto hast und dich auf dieser Website anmeldest, werden wir ein temporäres Cookie setzen, um festzustellen, ob dein Browser Cookies akzeptiert. Dieses Cookie enthält keine personenbezogenen Daten und wird verworfen, wenn du deinen Browser schließt.</p>
+<p>Wenn du dich anmeldest, werden wir einige Cookies einrichten, um deine Anmeldeinformationen und Anzeigeoptionen zu speichern. Anmelde-Cookies verfallen nach zwei Tagen und Cookies für die Anzeigeoptionen nach einem Jahr. Falls du bei der Anmeldung „Angemeldet bleiben“ auswählst, wird deine Anmeldung zwei Wochen lang aufrechterhalten. Mit der Abmeldung aus deinem Konto werden die Anmelde-Cookies gelöscht.</p>
+<p>Wenn du einen Artikel bearbeitest oder veröffentlichst, wird ein zusätzlicher Cookie in deinem Browser gespeichert. Dieser Cookie enthält keine personenbezogenen Daten und verweist nur auf die Beitrags-ID des Artikels, den du gerade bearbeitet hast. Der Cookie verfällt nach einem Tag.</p>
+
+<h2> 5. Rechte der betroffenen Person </h2>
+<p>Werden personenbezogene Daten von Ihnen verarbeitet, sind Sie Betroffener i.S.d. DSGVO und es stehen Ihnen folgende Rechte gegenüber dem Verantwortlichen zu:</p>
+<h4> Auskunftsrecht </h4> 
+<p>Sie können von dem Verantwortlichen eine Bestätigung darüber verlangen, ob personenbezogene Daten, die Sie betreffen, von uns verarbeitet werden. </p>
+<p>Liegt eine solche Verarbeitung vor, können Sie von dem Verantwortlichen über folgende Informationen Auskunft verlangen:</p>
+<p>die Zwecke, zu denen die personenbezogenen Daten verarbeitet werden;</p> 
+<p>(2) die Kategorien von personenbezogenen Daten, welche verarbeitet werden;</p>
+<p> (3) die Empfänger bzw. die Kategorien von Empfängern, gegenüber denen die Sie betreffenden personenbezogenen Daten offengelegt wurden oder noch offengelegt werden;</p> 
+<p> (4) die geplante Dauer der Speicherung der Sie betreffenden personenbezogenen Daten oder, falls konkrete Angaben hierzu nicht möglich sind, Kriterien für die Festlegung der Speicherdauer;</p>
+<p> (5) das Bestehen eines Rechts auf Berichtigung oder Löschung der Sie betreffenden personenbezogenen Daten, eines Rechts auf Einschränkung der Verarbeitung durch den Verantwortlichen oder eines Widerspruchsrechts gegen diese Verarbeitung; </p>
+<p>(6) das Bestehen eines Beschwerderechts bei einer Aufsichtsbehörde;</p>
+<p> (7) alle verfügbaren Informationen über die Herkunft der Daten, wenn die personenbezogenen Daten nicht bei der betroffenen Person erhoben werden;</p>
+<p> (8) das Bestehen einer automatisierten Entscheidungsfindung einschließlich Profiling gemäß Art.&nbsp;22 Abs.&nbsp;1 und 4 DSGVO und – zumindest in diesen Fällen – aussagekräftige Informationen über die involvierte Logik sowie die Tragweite und die angestrebten Auswirkungen einer derartigen Verarbeitung für die betroffene Person.</p>
+<p>Ihnen steht das Recht zu, Auskunft darüber zu verlangen, ob die Sie betreffenden personenbezogenen Daten in ein Drittland oder an eine internationale Organisation übermittelt werden. In diesem Zusammenhang können Sie verlangen, über die geeigneten Garantien gem. Art.&nbsp;46 DSGVO im Zusammenhang mit der Übermittlung unterrichtet zu werden.</p>
+<h4> Recht auf Berichtigung</h4> <p>Sie haben ein Recht auf Berichtigung und/oder Vervollständigung gegenüber dem Verantwortlichen, sofern die verarbeiteten personenbezogenen Daten, die Sie betreffen, unrichtig oder unvollständig sind. Der Verantwortliche hat die Berichtigung unverzüglich vorzunehmen.</p>
+<h4> Recht auf Einschränkung der Verarbeitung </h4>
+<p>Unter den folgenden Voraussetzungen können Sie die Einschränkung der Verarbeitung der Sie betreffenden personenbezogenen Daten verlangen:</p>
+<p> (1) wenn Sie die Richtigkeit der Sie betreffenden personenbezogenen für eine Dauer bestreiten, die es dem Verantwortlichen ermöglicht, die Richtigkeit der personenbezogenen Daten zu überprüfen;</p>
+<p> (2) die Verarbeitung unrechtmäßig ist und Sie die Löschung der personenbezogenen Daten ablehnen und stattdessen die Einschränkung der Nutzung der personenbezogenen Daten verlangen;</p>
+<p> (3) der Verantwortliche die personenbezogenen Daten für die Zwecke der Verarbeitung nicht länger benötigt, Sie diese jedoch zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen benötigen, oder</p>
+<p> (4) wenn Sie Widerspruch gegen die Verarbeitung gemäß Art.&nbsp;21 Abs.&nbsp;1 DSGVO eingelegt haben und noch nicht feststeht, ob die berechtigten Gründe des Verantwortlichen gegenüber Ihren Gründen überwiegen.</p> 
+<p>Wurde die Verarbeitung der Sie betreffenden personenbezogenen Daten eingeschränkt, dürfen diese Daten – von ihrer Speicherung abgesehen – nur mit Ihrer Einwilligung oder zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen oder zum Schutz der Rechte einer anderen natürlichen oder juristischen Person oder aus Gründen eines wichtigen öffentlichen Interesses der Union oder eines Mitgliedstaats verarbeitet werden.</p>
+<p>Wurde die Einschränkung der Verarbeitung nach den o.g. Voraussetzungen eingeschränkt, werden Sie von dem Verantwortlichen unterrichtet bevor die Einschränkung aufgehoben wird.</p>
+<h4> Recht auf Löschung </h4>
+<ol>
+<li> Löschungspflicht </li>
+</ol> 
+<p>Sie können von dem Verantwortlichen verlangen, dass die Sie betreffenden personenbezogenen Daten unverzüglich gelöscht werden, und der Verantwortliche ist verpflichtet, diese Daten unverzüglich zu löschen, sofern einer der folgenden Gründe zutrifft:</p>
+<p> (1) Die Sie betreffenden personenbezogenen Daten sind für die Zwecke, für die sie erhoben oder auf sonstige Weise verarbeitet wurden, nicht mehr notwendig.</p>
+<p> (2) Sie widerrufen Ihre Einwilligung, auf die sich die Verarbeitung gem. Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;a oder Art.&nbsp;9 Abs.&nbsp;2 lit.&nbsp;a DSGVO stützte, und es fehlt an einer anderweitigen Rechtsgrundlage für die Verarbeitung. </p>
+<p> (3) Sie legen gem. Art.&nbsp;21 Abs.&nbsp;1 DSGVO Widerspruch gegen die Verarbeitung ein und es liegen keine vorrangigen berechtigten Gründe für die Verarbeitung vor, oder Sie legen gem. Art.&nbsp;21 Abs.&nbsp;2 DSGVO Widerspruch gegen die Verarbeitung ein. </p>
+<p>(4) Die Sie betreffenden personenbezogenen Daten wurden unrechtmäßig verarbeitet. </p> <p> (5) Die Löschung der Sie betreffenden personenbezogenen Daten ist zur Erfüllung einer rechtlichen Verpflichtung nach dem Unionsrecht oder dem Recht der Mitgliedstaaten erforderlich, dem der Verantwortliche unterliegt. </p>
+<p> (6) Die Sie betreffenden personenbezogenen Daten wurden in Bezug auf angebotene Dienste der Informationsgesellschaft gemäß Art.&nbsp;8 Abs.&nbsp;1 DSGVO erhoben.</p> <ol><li> Information an Dritte </li></ol> <p>Hat der Verantwortliche die Sie betreffenden personenbezogenen Daten öffentlich gemacht und ist er
+gem. Art.&nbsp;17 Abs.&nbsp;1 DSGVO zu deren Löschung verpflichtet, so trifft er unter Berücksichtigung der verfügbaren Technologie und der Implementierungskosten angemessene Maßnahmen, auch technischer Art, um für die Datenverarbeitung Verantwortliche, die die personenbezogenen Daten verarbeiten, darüber zu informieren, dass Sie als betroffene Person von ihnen die Löschung aller Links zu diesen personenbezogenen Daten oder von Kopien oder Replikationen dieser personenbezogenen Daten verlangt haben. </p>
+<ol>
+<li> Ausnahmen </li>
+</ol> 
+<p>Das Recht auf Löschung besteht nicht, soweit die Verarbeitung erforderlich ist</p> <p>(1) zur Ausübung des Rechts auf freie Meinungsäußerung und Information;</p>
+<p> (2) zur Erfüllung einer rechtlichen Verpflichtung, die die Verarbeitung nach dem Recht der Union oder der Mitgliedstaaten, dem der Verantwortliche unterliegt, erfordert, oder zur Wahrnehmung einer Aufgabe, die im öffentlichen Interesse liegt oder in Ausübung öffentlicher Gewalt erfolgt, die dem Verantwortlichen übertragen wurde;</p>
+<p> (3) aus Gründen des öffentlichen Interesses im Bereich der öffentlichen Gesundheit gemäß Art.&nbsp;9 Abs.&nbsp;2 lit.&nbsp;h und i sowie Art.&nbsp;9 Abs.&nbsp;3 DSGVO;</p>
+<p> (4) für im öffentlichen Interesse liegende Archivzwecke, wissenschaftliche oder historische Forschungszwecke oder für statistische Zwecke gem. Art.&nbsp;89 Abs.&nbsp;1 DSGVO, soweit das unter Abschnitt a) genannte Recht voraussichtlich die Verwirklichung der Ziele dieser Verarbeitung unmöglich macht oder ernsthaft beeinträchtigt, oder</p>
+<p>(5) zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen.</p>
+<h4> Recht auf Unterrichtung </h4>
+<p>Haben Sie das Recht auf Berichtigung, Löschung oder Einschränkung der Verarbeitung gegenüber dem Verantwortlichen geltend gemacht, ist dieser verpflichtet, allen Empfängern, denen die Sie betreffenden personenbezogenen Daten offengelegt wurden, diese Berichtigung oder Löschung der Daten oder Einschränkung der Verarbeitung mitzuteilen, es sei denn, dies erweist sich als unmöglich oder ist mit einem unverhältnismäßigen Aufwand verbunden.</p> 
+<p>Ihnen steht gegenüber dem Verantwortlichen das Recht zu, über diese Empfänger unterrichtet zu werden.</p>
+<h4> Widerspruchsrecht </h4> 
+<p>Sie haben das Recht, aus Gründen, die sich aus ihrer besonderen Situation ergeben, jederzeit gegen die Verarbeitung der Sie betreffenden personenbezogenen Daten, die aufgrund von Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;e oder f DSGVO erfolgt, Widerspruch einzulegen; dies gilt auch für ein auf diese Bestimmungen gestütztes Profiling. </p>
+<p>Der Verantwortliche verarbeitet die Sie betreffenden personenbezogenen Daten nicht mehr, es sei denn, er kann zwingende schutzwürdige Gründe für die Verarbeitung nachweisen, die Ihre Interessen, Rechte und Freiheiten überwiegen, oder die Verarbeitung dient der Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen.</p> 
+<p>Werden die Sie betreffenden personenbezogenen Daten verarbeitet, um Direktwerbung zu betreiben, haben Sie das Recht, jederzeit Widerspruch gegen die Verarbeitung der Sie betreffenden personenbezogenen Daten zum Zwecke derartiger Werbung einzulegen; dies gilt auch für das Profiling, soweit es mit solcher Direktwerbung in Verbindung steht.</p>
+<p>Widersprechen Sie der Verarbeitung für Zwecke der Direktwerbung, so werden die Sie betreffenden personenbezogenen Daten nicht mehr für diese Zwecke verarbeitet.</p>
+<p>Sie haben die Möglichkeit, im Zusammenhang mit der Nutzung von Diensten der Informationsgesellschaft – ungeachtet der Richtlinie 2002/58/EG – Ihr Widerspruchsrecht mittels automatisierter Verfahren auszuüben, bei denen technische Spezifikationen verwendet werden.</p> 
+<h4> Recht auf Widerruf der datenschutzrechtlichen Einwilligungserklärung </h4>
+<p>Sie haben das Recht, Ihre datenschutzrechtliche Einwilligungserklärung jederzeit zu widerrufen. Durch den Widerruf der Einwilligung wird die Rechtmäßigkeit der aufgrund der Einwilligung bis zum Widerruf erfolgten Verarbeitung nicht berührt.</p> 
+<h4> Recht auf Beschwerde bei einer Aufsichtsbehörde </h4>
+<p>Unbeschadet eines anderweitigen verwaltungsrechtlichen oder gerichtlichen Rechtsbehelfs steht Ihnen das Recht auf Beschwerde bei einer Aufsichtsbehörde, insbesondere in dem Mitgliedstaat ihres Aufenthaltsorts, ihres Arbeitsplatzes oder des Orts des mutmaßlichen Verstoßes, zu, wenn Sie der Ansicht sind, dass die Verarbeitung der Sie betreffenden personenbezogenen Daten gegen die DSGVO verstößt. </p>
+<p>Die Aufsichtsbehörde, bei der die Beschwerde eingereicht wurde, unterrichtet den Beschwerdeführer über den Stand und die Ergebnisse der Beschwerde einschließlich der Möglichkeit eines gerichtlichen Rechtsbehelfs nach Art.&nbsp;78 DSGVO.</p> </div>
+    </div>
+</div>
+<?php
+include("footer.php");
+?>
