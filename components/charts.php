80896398454f0bb015a159c8c773768331aa32a4
Nucleus <58828815+nucleus-ffm@users.noreply.github.com>
2022-03-25 11:03:24 +0100

    Update charts.php

--- components/charts.php
+++ components/charts.php
@@ -8,7 +8,8 @@
     <canvas id="myChart"></canvas>
 </div>
 
-<script src="libraries/chartjs/chartjs/dist/Chart.js"></script>
+// <script src="libraries/chartjs/chartjs/dist/Chart.js"></script> 
+
 <script>
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
